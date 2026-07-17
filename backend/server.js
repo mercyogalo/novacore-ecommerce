68fea0f6a433185051af08ad3bdf9933ed36ddb1
Mercy Ogalo <ogalomercy8@gmail.com>
2026-07-17 23:49:10 +0300

    Added the connections for the backend

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,19 @@ const mongoose=require("mongoose");
 const dotenv=require("dotenv");
 const cors=require("cors");
 const app=express();
+
+const PORT=process.env.PORT || 5000;
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI,
+    {})
+.then(()=>console.log("MongoDB connected"))
+.then((error)=>console.log(error));
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on port ${PORT}`);
+});
